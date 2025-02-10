a68534d1dddd56284ca2ad367dba0bcbc881e87e
John Foerch <jjfoerch@gmail.com>
2025-02-10 15:28:53 -0500

    openspace-lua-repl.el: further work

--- bin/openspace-lua-repl.el
+++ bin/openspace-lua-repl.el
@@ -1,10 +1,18 @@
 
 (require 'lua-mode)
 
-(defvar openspace-lua-repl-script
-  (concat (file-name-as-directory (getenv "HOME"))
-          "Programs/OpenSpace/jjf-openspacerc/bin/openspace-lua-repl.lua"))
+(defun openspace-lua-repl-asset (relpath)
+  "Resolve RELPATH against the location of openspace-lua-repl.el.
+Used for the Lua client process."
+  (let ((openspace-lua-repl-path
+         (file-name-directory
+          (or load-file-name
+              (locate-library "openspace-lua-repl")))))
+    (concat openspace-lua-repl-path "/" relpath)))
 
 (defun openspace-lua-repl ()
   (interactive)
-  (lua-start-process "OpenSpace" nil nil openspace-lua-repl-script))
+  (lua-start-process "OpenSpace" nil nil
+                     (openspace-lua-repl-asset "openspace-lua-repl.lua")))
+
+(provide 'openspace-lua-repl)
